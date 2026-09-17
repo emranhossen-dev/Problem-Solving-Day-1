2cfb21d0d1c0261b51b9a2c4af7077cb36e221eb
Emran Hossen <dev.emranhossen@gmail.com>
2026-09-17 21:55:21 +0600

    Add isEven function to check even or odd numbers

---
+++ problem2.js
@@ -0,0 +1,8 @@
+/*
+Problem 2: Check Even or Odd
+Write a function isEven(n) that returns true if a number is even, and false if it is odd.
+Example: 4 -> true, 7 -> false
+*/
+function isEven(n) {
+  return n % 2 === 0;
+}
